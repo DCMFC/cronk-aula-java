6a79b5bc8ae558e48bee244680ee2d2575501ff8
Dayane Caldeira <d.caldeira@mozio.com>
2025-04-05 18:20:17 -0300

    feat: add conversion methods

---
+++ conversion/src/main/java/conversion/example/App.java
@@ -0,0 +1,27 @@
+package conversion.example;
+
+import java.io.IOException;
+
+/**
+ * Main class.
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Main method.
+     */
+    public static void main(String[] args) {
+        String path = "conversion/src/main/java/conversion/example/images/image-1.jpg";
+        ImageConversion imageConversion = new ImageConversion();
+        try {
+            String base64Image = imageConversion.convertImageToBase64(path);
+            System.out.println(base64Image);
+
+            imageConversion.convertBase64ToImage(base64Image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
