f7986b14d17b3b041f714198a10b467cb1bc3bb4
Dayane Caldeira <d.caldeira@mozio.com>
2025-04-12 17:52:00 -0300

    unit tests

--- conversion/src/test/java/conversion/example/AppTest.java
+++ conversion/src/test/java/conversion/example/AppTest.java
@@ -1,16 +1,18 @@
 package conversion.example;
 
+import java.io.File;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 class AppTest {
     /**
-     * Success Test.
+     * Test to convert from file to base64.
      */
     @Test
     public void fileToBase64StringConversion() throws IOException {
@@ -19,5 +21,20 @@ class AppTest {
         String result = imageConversion.convertImageToBase64(path);
 
         assertNotNull(result);
+    }
+
+    /**
+     * Test to convert base64 to file.
+     */
+    @Test
+    public void base64ToFileConversion() throws IOException {
+        String path = "src/test/java/conversion/example/test.jpg";
+        ImageConversion imageConversion = new ImageConversion();
+        String result = imageConversion.convertImageToBase64(path);
+
+        imageConversion.convertBase64ToImage(result);
+        String expectedOutput = "image-output.jpg";
+        File image = new File(expectedOutput);
+        assertTrue(image.exists());
     }
 }
